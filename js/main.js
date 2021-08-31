7bcb72e079eaf8b35f2924055ca801f0bc4a75fb
Fur1ouS <furiticus@gmail.com>
2021-08-31 15:33:34 +0300

    add range filter

--- js/main.js
+++ js/main.js
@@ -71,6 +71,9 @@ window.onload = function (){
            coordYprocent = coordY / parallaxHeight * 100;
         });
     }
+
+    slideOne();
+    slideTwo();
 }
 
 // header menu btn
@@ -103,6 +106,42 @@ $(document).ready(function() {
         $('.menu-list').removeClass('menu-active');
         $('.menu-list-second').removeClass('menu-active');
     });
-
-
 });
+
+// slider range
+let sliderRange = document.getElementById('range-like');
+let valueLikes = document.getElementById('value-like');
+
+valueLikes.innerHTML = sliderRange.value;
+
+sliderRange.oninput = function () {
+    valueLikes.innerHTML = this.value;
+}
+
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 0;
+let sliderTrack = document.querySelector(".time-slider-track");
+let sliderMaxValue = document.getElementById("slider-1").max;
+
+function slideOne(){
+    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+        sliderOne.value = parseInt(sliderTwo.value) - minGap;
+    }
+    displayValOne.textContent = sliderOne.value;
+    fillColor();
+}
+function slideTwo(){
+    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+        sliderTwo.value = parseInt(sliderOne.value) + minGap;
+    }
+    displayValTwo.textContent = sliderTwo.value;
+    fillColor();
+}
+function fillColor(){
+    percent1 = (sliderOne.value / sliderMaxValue) * 100;
+    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+}
